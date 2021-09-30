1e77ba3f38cb71db9b663f7c6b42040835103f98
Jhonatan Freitas <itjhonatan01@gmail.com>
2021-09-30 15:15:05 -0300

    create modal

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const openModal = () => {
+    document.querySelector('.modal-overlay').classList.add('active')
+    
+}
+
+const closeModal = () => {
+    document.querySelector('.modal-overlay').classList.remove('active')
+    
+}
